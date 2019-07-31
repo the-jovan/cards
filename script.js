b81d461ba869de9656e9e0c9767f47e0e228baae
the-jovan <jovanjocic1993@gmail.com>
2019-07-31 15:48:05 +0200

    Added code stuff

---
+++ script.js
@@ -0,0 +1,31 @@
+const initialRotation = [0, -1, -2, -3, -4, -5, -6, -7]
+
+const cards = Object.values(document.querySelectorAll('.card'))
+
+for ( let i = 0; i < initialRotation.length; i++) {
+  cards[i].style.transform = `rotate(${initialRotation[i]}deg)`
+  cards[i].onclick = () => rotate(cards.indexOf(cards[i]))
+}
+
+
+function rotate(index) {
+  let delta = 165;
+
+  cards.forEach((item, ind) => { 
+			if (ind < index) {
+        item.style.transform = `rotate(${delta}deg)`
+        delta -= 10
+      }
+			else
+				if (ind == index) {
+          delta = 0
+          item.style.transform = `rotate(${delta}deg)`
+          delta =- 10
+        }
+				else {
+          item.style.transform = `rotate(${delta}deg)`
+          delta-=10
+        }
+			})
+  
+}
